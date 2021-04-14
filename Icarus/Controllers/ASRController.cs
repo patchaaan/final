b85c081fcdc4cb25fec2ab888935f355c865a803
monica835 <monica.dinglasa@student.passerellesnumeriques.org>
2021-04-15 06:32:28 +0800

    fdafda

---
+++ Icarus/Controllers/ASRController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Rotativa;
+using Icarus.Models;
+using System.Web.Mvc;
+namespace Icarus.Controllers
+{
+    public class ASRController : Controller
+    {
+        // GET: ASR
+        public ActionResult Index()
+        {
+            using (ICARUSDBEntities db = new ICARUSDBEntities())
+            {
+                var employeeList = db.vrptAssertionSummaries.ToList();
+                return View(employeeList);
+            }
+        }
+
+        //Convert Index Page as PDF
+        public ActionResult PrintViewToPdf()
+        {
+            var report = new ActionAsPdf("Index");
+            return report;
+        }
+    }
+}
