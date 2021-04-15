902e59cfb5a5865a2daa11218f25bda2718e30e5
monica835 <monica.dinglasa@student.passerellesnumeriques.org>
2021-04-15 10:43:32 +0800

    monica

---
+++ Icarus/Controllers/UnbilledtoCodepController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rotativa;
+using Icarus.Models;
+namespace Icarus.Controllers
+{
+    public class UnbilledtoCodepController : Controller
+    {
+        // GET: UnbilledtoCodep
+        public ActionResult Index()
+        {
+            using (ICARUSDBEntities db = new ICARUSDBEntities())
+            {
+                var employeeList = db.vrptUnderChargedExpenses.ToList();
+                return View(employeeList);
+            }
+        }
+
+        //Convert Index Page as PDF
+        public ActionResult PrintViewToPdf()
+        {
+            var report = new ActionAsPdf("Index");
+            return report;
+        }
+    }
+}
